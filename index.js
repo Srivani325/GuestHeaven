757588a80339017743dcbd475a423aecd7ce7c02
Srivani <gogulasrivani6@gmail.com>
2025-08-11 17:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function main() {
 app.use((req,res,next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+    res.locals.currUser = isAuthenticated() ? req.user : null;
     next();
 })
 
